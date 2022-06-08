5471cf8d0adfe992af4c38bc980de490361d0367
Lázaro Alves <geo.eolo@gmail.com>
2022-06-07 23:47:44 -0300

    feature/implement component Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,43 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { Feather } from '@expo/vector-icons';
 
 export default function Home(){
+
+    const navigation = useNavigation(); 
+
     return(
-        <View style={styles.container}>
-            <Text>Página HOME</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            
+            <View style={styles.header}>
+                <Text style={styles.name}>DevBlog</Text>
+
+                <TouchableOpacity>
+                    <Feather name="search" size={24} color="#FFF"/>
+                </TouchableOpacity>
+            </View>
+
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: '#232630'
+    },
+    header: {
+        flexDirection: "row",
         alignItems: 'center',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
+        marginHorizontal: 18,
+        marginTop: 18,
+        marginBottom: 24,
+    },
+    name: {
+        fontSize: 28,
+        color: '#FFF',
+        fontWeight: 'bold',
     },
 })
