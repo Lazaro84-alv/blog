e4db600335d3a6864d5e49b39866017d32cd7231
Lázaro Alves <geo.eolo@gmail.com>
2022-06-11 10:52:47 -0300

    feature/implement navigation between screens

--- src/components/CategoryItem/index.js
+++ src/components/CategoryItem/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
+import { useNavigation } from '@react-navigation/native';
+
 export default function CategoryItem({ data }) {
+    const navigation = useNavigation();
+
+    function handleNavigate() {
+        navigation.navigate("Category", {id: data.id, title: data?.attributes?.name});
+    }
+
     return (
         <TouchableOpacity 
             style={styles.container}
             activeOpacity={0.9}
+            onPress={handleNavigate}
         >
             <Image 
                 style={styles.icon}
